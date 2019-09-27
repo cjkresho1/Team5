79a392f5f97eb1a389921642250732dc2edeb841
Austin Napier <45158162+CakeIzGood@users.noreply.github.com>
2019-09-27 00:36:13 -0400

    Start sell and display methods
    Outline basic functionality for sell and display. sell still needs to be formatted and include the time of sale, display still needs to be formatted.

--- Project1/src/Warehouse.java
+++ Project1/src/Warehouse.java
@@ -4,154 +4,150 @@ import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-public class Warehouse 
-{
+public class Warehouse {
 	private LinkedList<BikePart> parts;
-	
-	public Warehouse()
-	{
+
+	public Warehouse() {
 		parts = new LinkedList<BikePart>();
 	}
-	
-	public Warehouse(String dbFile)
-	{
+
+	public Warehouse(String dbFile) {
 		parts = new LinkedList<BikePart>();
 		File file = new File(dbFile);
-		
-		if (file.exists())
-		{
-			try 
-			{
+
+		if (file.exists()) {
+			try {
 				Scanner scan = new Scanner(file);
-				while(scan.hasNextLine())
-				{
+				while (scan.hasNextLine()) {
 					String temp = scan.nextLine();
 					String[] data = temp.split(",");
-					
-					parts.add(new BikePart(data[0], Integer.parseInt(data[1]), 
-							Double.parseDouble(data[2]), Double.parseDouble(data[3]), 
-							Boolean.getBoolean(data[4]), Integer.parseInt(data[5])));
+
+					parts.add(new BikePart(data[0], Integer.parseInt(data[1]), Double.parseDouble(data[2]),
+							Double.parseDouble(data[3]), Boolean.getBoolean(data[4]), Integer.parseInt(data[5])));
 				}
-			} 
-			catch (FileNotFoundException e) 
-			{
+				scan.close();
+			} catch (FileNotFoundException e) {
 				System.out.print("HOW DID YOU DO THIS");
 				System.exit(1);
-				//THIS SHOULD NEVER EVER HAPPEN BUT IF IT DOES...HOW?
+				// THIS SHOULD NEVER EVER HAPPEN BUT IF IT DOES...HOW?
 			}
-			
+
 		}
 	}
-	
-	public boolean read(String file)
-	{
-		/*TODO use file to update parts database. Create new entries, and 
-		 * update existing ones. Quantity are additive. 
+
+	public boolean read(String file) {
+		/*
+		 * TODO use file to update parts database. Create new entries, and update
+		 * existing ones. Quantity are additive.
 		 */
-		
+
 		return false;
 	}
-	
-	public boolean enter(String info)
-	{
-		/*TODO parse part info, and add part to parts database. If part already
-		 * exists, update existing one.
+
+	public boolean enter(String info) {
+		/*
+		 * TODO parse part info, and add part to parts database. If part already exists,
+		 * update existing one.
 		 */
-		
+
 		return false;
 	}
-	
-	public String sell(int partNum)
-	{
-		/*TODO find part based on partNum. Return a string with:
-		 * Name and Price, Sale Status, and Time of Sale
-		 * Decrement part quantity.
-		 * If the part doesn't exist, return an empty string to indicate failure
+
+	public String sell(int partNum) {
+		String partInfo = null;
+		for (int i = 0; i < parts.size(); i++) {
+			if (partNum == parts.get(i).getNum()) {
+				partInfo = parts.get(i).getName() + " " + parts.get(i).getPrice() + " " + parts.get(i).isOnSale() + " ";
+				parts.get(i).setQuantity(parts.get(i).getQuantity()-1);
+			}
+		}
+		if (partInfo == null) {
+			partInfo = "";
+		}
+		/*
+		 * TODO format partInfo and add time of sale to end
 		 */
-		
-		return "";
+
+		return partInfo;
 	}
-	
+
 	public String display(String partName)
+	/*
+	 * TODO make sure partInfo is formatted acceptably
+	 */
 	{
-		/*TODO find part based on partName. Return a string with:
-		 * Name and Price
-		 * If the part doesn't exist, return an empty string to indicate failure
-		 */
-		
-		return "";
+		String partInfo = null;
+		for (int i = 0; i < parts.size(); i++) {
+			if (parts.get(i).getName() == partName) {
+				partInfo = partName + " " + parts.get(i).getPrice();
+			}
+		}
+		if (partInfo == null) {
+			partInfo = "";
+		}
+		return partInfo;
 	}
-	
-	public String[] sortName()
-	{
+
+	public String[] sortName() {
 		String[] val = new String[parts.size()];
-		
-		
-		/* This sorts the warehouse inventory based on Name. This is accomplished by 
+
+		/*
+		 * This sorts the warehouse inventory based on Name. This is accomplished by
 		 * using the built in .sort(Comparator<T>) method. The Comparator<BikePart>
-		 * passed to the method is a custom, overridden Comparator that is designed 
-		 * to compare the Names.
+		 * passed to the method is a custom, overridden Comparator that is designed to
+		 * compare the Names.
 		 */
-		parts.sort(new Comparator<BikePart>() 
-		{
+		parts.sort(new Comparator<BikePart>() {
 			@Override
-			public int compare(BikePart b1, BikePart b2)
-			{
+			public int compare(BikePart b1, BikePart b2) {
 				return b1.getName().compareTo(b2.getName());
 			}
-		}	
-		);
-		
-		for (int i = 0; i > parts.size(); i++)
-		{
+		});
+
+		for (int i = 0; i > parts.size(); i++) {
 			val[i] = parts.get(i).toString();
 		}
-		
+
 		return val;
 	}
-	
-	
-	public String[] sortNumber()
-	{
+
+	public String[] sortNumber() {
 		String[] val = new String[parts.size()];
-		
-		
-		/* This sorts the warehouse inventory based on PartNumber. This is accomplished 
-		 * by using the built in .sort(Comparator<T>) method. The 
-		 * Comparator<BikePart> passed to the method is a custom, overridden 
-		 * Comparator that is designed to compare the Names.
+
+		/*
+		 * This sorts the warehouse inventory based on PartNumber. This is accomplished
+		 * by using the built in .sort(Comparator<T>) method. The Comparator<BikePart>
+		 * passed to the method is a custom, overridden Comparator that is designed to
+		 * compare the Names.
 		 */
-		parts.sort(new Comparator<BikePart>() 
-		{
+		parts.sort(new Comparator<BikePart>() {
 			@Override
-			public int compare(BikePart b1, BikePart b2)
-			{
+			public int compare(BikePart b1, BikePart b2) {
 				return b1.getNum() - b2.getNum();
 			}
-		}	
-		);
-		
-		for (int i = 0; i > parts.size(); i++)
-		{
+		});
+
+		for (int i = 0; i > parts.size(); i++) {
 			val[i] = parts.get(i).toString();
 		}
-		
+
 		return val;
 	}
-	
-	public void quit(String file)
-	{
-		/*TODO update file to reflect current warehouse database. If file
-		 * exists, delete content and overwrite. 
+
+	public void quit(String file) {
+
+		/*
+		 * TODO update file to reflect current warehouse database. If file exists,
+		 * delete content and overwrite.
 		 */
 	}
-	
-	private boolean parseFile(String file)
-	{
-		/*TODO use file to update parts database. Create new entries, and 
-		 * update existing ones.
+
+	private boolean parseFile(String file) {
+		/*
+		 * TODO use file to update parts database. Create new entries, and update
+		 * existing ones.
 		 */
-		
+
 		return false;
 	}
 }
