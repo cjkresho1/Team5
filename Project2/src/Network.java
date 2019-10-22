f6a209c26edfe83824641664d54cc7de34d9a1a0
cjkresho1 <cjkresho@gmail.com>
2019-10-22 16:01:46 -0400

    Created method signatures for Network

---
+++ Project2/src/Network.java
@@ -0,0 +1,86 @@
+import java.util.LinkedList;
+
+/**
+ * 
+ */
+
+/**
+ * @author Team5
+ *
+ */
+public class Network 
+{
+	private Warehouse warehouse;
+	private LinkedList<Warehouse> vans;
+	
+	
+	public Network()
+	{
+		
+	}
+	
+	
+	public Network(String filename)
+	{
+		
+	}
+	
+	
+	public boolean deliver(String filename)
+	{
+		return false;
+	}
+	
+	
+	public boolean add(String part)
+	{
+		return false;
+	}
+	
+	
+	public String display(int partNum)
+	{
+		return "";
+	}
+	
+	public String display(String partName)
+	{
+		return "";
+	}
+	
+	
+	public String sell(String partName)
+	{
+		return "";
+	}
+	
+	
+	public String[] sortName(String warehouse)
+	{
+		return null;
+	}
+	
+	
+	public String[] sortNum(String warehouse)
+	{
+		return null;
+	}
+	
+	
+	public boolean addVan(String vanName)
+	{
+		return false;
+	}
+	
+	
+	public boolean transfer(String filename)
+	{
+		return false;
+	}
+	
+	
+	public boolean quit(String filename)
+	{
+		return false;
+	}
+}
